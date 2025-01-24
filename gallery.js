0a57cd400f8a1b86295c0924c3f34c98f7488fd5
hassan ali <hassanali0111272@gmail.com>
2025-01-24 23:55:00 +0500

    modal template added

--- gallery.js
+++ gallery.js
@@ -1,1 +1,13 @@
+const showMemoriesData = async () => {
+  try {
+    const { data, error } = await supabase.from("countries").select();
+    if (error) throw error;
+    if (data) {
+      console.log(data);
+    }
+  } catch (error) {
+    console.log(error)
+  }
+};
 
+// window.onload = showMemoriesData();
