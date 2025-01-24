e97858a21aad22bad6ef743a9b821692c52440bc
hassan ali <hassanali0111272@gmail.com>
2025-01-24 23:21:58 +0500

    dummy data gallery

---
+++ config.js
@@ -0,0 +1,10 @@
+// console.log(supabase)
+
+const { createClient } = supabase;
+
+const supabaseUrl = "https://hpnhxannetfthfsdcehp.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhwbmh4YW5uZXRmdGhmc2RjZWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjgxODQsImV4cCI6MjA1MzMwNDE4NH0.XmbaaBzzanaUYl5J3n9oa9VohqtmISVDHeEGxwEwTx4";
+const supabaseClient = createClient(supabaseUrl, supabaseKey);
+
+window.supabase = supabaseClient;
