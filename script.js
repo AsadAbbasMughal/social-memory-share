ffe5b87828f2f389fafb27ab2e20e0bbc61f1e20
Asim Siddiqui <asimsiddiqui939@gmail.com>
2025-01-25 10:22:06 -0800

    Authentication work

--- script.js
+++ script.js
@@ -1,6 +1,72 @@
-let signupEmail = document.querySelector("#signupEmail")
-let signupPassword = document.querySelector("#signupPassword")
-let signupBtn = document.querySelector("#signupBtn")
-let signupContainer = document.querySelector("#signupContainer")
-let switchToSignup = document.querySelector(".switchToSignup")
-let switchToSignip = document.querySelector(".switchToSignip")
+const signupEmail = document.querySelector("#signupEmail")
+const signupPassword = document.querySelector("#signupPassword")
+const signinEmail = document.querySelector("#signinEmail");
+const signinpPassword = document.querySelector("#signinpPassword");
+const signupBtn = document.querySelector("#signupBtn")
+const signinpBtn = document.querySelector("#signinpBtn")
+const signupContainer = document.querySelector("#signupContainer")
+const signinpContainer = document.querySelector("#signinpContainer")
+const switchToSignup = document.querySelector(".switchToSignup")
+const switchToSignip = document.querySelector(".switchToSignip")
+
+async function signup(){
+    try {
+        event.preventDefault();
+        const { data, error } = await supabase.auth.signUp({
+            email: signupEmail.value,
+            password: signupPassword.value,
+          })
+          
+          if (error) throw error;
+
+          if(data){
+            alert("PLease check your email for your Confirmation");
+            console.log(data);
+            signupContainer.style.display = "none";
+            signinpContainer.style.display = "block";
+            
+                      Swal.fire({
+                        title: "Sign Up",
+                        text: "Sign Up Successfully",
+                        icon: "success",
+                      });
+          }
+          return data;
+
+
+    } catch (error) {
+        console.log("Error is : " + error.message);
+    }
+};
+
+async function signin(){
+    try {
+        event.preventDefault();
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: signinEmail.value,
+            password: signinpPassword.value,
+          })
+
+          if(error) throw error;
+          if(data){
+            console.log(data);
+            // window.location.href = "";
+            Swal.fire({
+                title: "Sign In",
+                text: "Sign In Successfully",
+                icon: "success",
+              });
+          }
+          return data;
+    } catch (error) {
+        console.log("Error is : " + error.message);
+    }
+}
+
+if(signupBtn){
+    signupBtn.addEventListener("click" , signup);
+}
+
+if(signinpBtn){
+    signinpBtn.addEventListener("click" , signin);
+}
