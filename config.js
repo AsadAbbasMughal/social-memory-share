11ef6d0a642b3afdd31140ce910c900e31bb113c
Muhammad Saad <studentsaad41@gmail.com>
2025-01-28 17:10:36 +0500

    Update config.js
    Change supabase url and key to connect main supabase project

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const {createClient} = supabase;
 
-const supabaseUrl = 'https://tgeouiprsvwtrccffqxe.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRnZW91aXByc3Z3dHJjY2ZmcXhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjYxMDQsImV4cCI6MjA1MzMwMjEwNH0.LL6nRJSzfZRW7QnCPPiGIm77MlOZGfc8dUdTEFybDrQ'
+const supabaseUrl = 'https://hpnhxannetfthfsdcehp.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhwbmh4YW5uZXRmdGhmc2RjZWhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3MjgxODQsImV4cCI6MjA1MzMwNDE4NH0.XmbaaBzzanaUYl5J3n9oa9VohqtmISVDHeEGxwEwTx4'
 const supabaseCreate = createClient(supabaseUrl, supabaseKey)
 
 window.supabase = supabaseCreate;
